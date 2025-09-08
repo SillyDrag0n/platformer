155959a33e47a4bff2be267d9f2059e9e5387dc7
SillyDrag0n <silvanmueller2004@gmx.ch>
2025-09-08 15:12:26 +0200

    add sample tileset and assets for testing

---
+++ scenes/components/shoot_component.gd
@@ -0,0 +1,9 @@
+class_name ShootComponent
+extends Node
+
+@export_subgroup("Nodes")
+@export var shoot_delay_timer: Timer
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
