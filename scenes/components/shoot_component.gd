faa9dfcb9dcd2d1e82f2cdc0e1d46a282d43058d
SillyDrag0n <silvanmueller2004@gmx.ch>
2025-09-20 13:45:53 +0200

    add changes from template project

--- scenes/components/shoot_component.gd
+++
@@ -1,9 +0,0 @@
-class_name ShootComponent
-extends Node
-
-@export_subgroup("Nodes")
-@export var shoot_delay_timer: Timer
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
