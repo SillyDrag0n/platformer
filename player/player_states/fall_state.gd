8282ff4a7984d7ad50cd5b4558f3cc5a8af14a4e
SillyDrag0n <silvanmueller2004@gmx.ch>
2025-10-07 21:16:17 +0200

    modify fall animation and state to move horizontally while falling

--- player/player_states/fall_state.gd
+++ player/player_states/fall_state.gd
@@ -2,6 +2,8 @@ extends NodeState
 
 @export var character_body_2d : CharacterBody2D
 @export var animated_sprite_2d : AnimatedSprite2D
+@export var jump_horizontal_speed : int = 200
+@export var max_jump_horizontal_speed : int = 200	
 
 @export_category("Fall State")
 @export var coyote_time : float = 0.1
@@ -15,9 +17,18 @@ func on_process(delta : float):
 
 
 func on_physics_process(delta : float):
+	var direction : float = GameInputEvents.movement_input()
+	
 	if !character_body_2d.is_on_floor():
 		get_coyote_time()
 		character_body_2d.velocity.y += GRAVITY * delta
+
+		if direction != 0:
+			animated_sprite_2d.flip_h = false if direction > 0 else true
+			character_body_2d.velocity.x += direction * jump_horizontal_speed
+			character_body_2d.velocity.x = clamp(character_body_2d.velocity.x, -max_jump_horizontal_speed, max_jump_horizontal_speed)
+		else:
+			character_body_2d.velocity.x = 0
 	
 	character_body_2d.move_and_slide()
 	
