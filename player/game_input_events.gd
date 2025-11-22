d29c18536470ceef05231eaadd3d5c7bc859021d
SillyDrag0n <silvanmueller2004@gmx.ch>
2025-11-22 12:28:15 +0100

    add saloon and interact key

--- player/game_input_events.gd
+++ player/game_input_events.gd
@@ -35,3 +35,7 @@ static func wall_cling_input() -> bool:
 static func reload_input() -> bool: 
 	var reload_input : bool = Input.is_action_just_pressed("reload")
 	return reload_input
+
+static func interact_input() -> bool: 
+	var interact_input : bool = Input.is_action_just_pressed("interact")
+	return interact_input
