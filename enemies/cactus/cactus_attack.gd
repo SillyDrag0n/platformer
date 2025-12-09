118e8398d1cd7fbb1bbcc407699ea6cdc815461c
SillyDrag0n <silvanmueller2004@gmx.ch>
2025-12-09 10:52:45 +0100

    add common bullet to enemies folder

--- enemies/cactus/cactus_attack.gd
+++ enemies/cactus/cactus_attack.gd
@@ -1,6 +1,6 @@
 extends AnimatedSprite2D
 
-var bullet_impact_effect = preload("res://player/gun/bullet/ui/bullet_impact_effect.tscn")
+var bullet_impact_effect = preload("res://enemies/_common/bullet/bullet_impact_effect.tscn")
 
 @export var speed : int = 350
 @export var damage_amount : int = 1
