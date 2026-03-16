d674b7a8226424fef0a4cfed180274d44a8894a5
SillyDrag0n <silvanmueller2004@gmx.ch>
2026-03-16 14:33:48 +0100

    fix boss movement

--- enemies/bosses/sand_spirit/state_machine/move_state.gd
+++ enemies/bosses/sand_spirit/state_machine/move_state.gd
@@ -1,23 +1,28 @@
 extends NodeState
 
-@export var StateMachine: NodeFiniteStateMachine
 @export var Boss: CharacterBody2D
 
-func physics_update(_delta):
+func on_physics_process(_delta):
+	print("physics update move")
 	var direction = (Boss.hover_target - Boss.global_position).normalized()
 	Boss.velocity = direction * Boss.hover_speed
 	
 	if Boss.global_position.distance_to(Boss.hover_target) < 20:
 		pick_new_hover_target()
 
-
 func enter():
 	pick_new_hover_target()
+	print("entered state move")
+
+
+func exit():
+	pass
 
 
 func pick_new_hover_target():
-	Boss.hover_target = Vector2(
-		randf_range(Boss.arena_left, Boss.arena_right),
-		randf_range(Boss.arena_top, Boss.arena_bottom)
-	)
+	Boss.hover_target = PlayerManager.player.global_position
+	# Boss.hover_target = Vector2(
+	# 	randf_range(Boss.arena_left, Boss.arena_right),
+	# 	randf_range(Boss.arena_top, Boss.arena_bottom)
+	# )
 	print(Boss.hover_target)
--- enemies/skeleton/state_machine/attack_state.gd
+++ enemies/skeleton/state_machine/attack_state.gd
@@ -3,7 +3,6 @@ extends NodeState
 @export var character_body_2d: CharacterBody2D
 @export var animated_sprite_2d: AnimatedSprite2D
 
-func _physics_process(_delta: float) -> void:
 
 func enter():
 	animated_sprite_2d.play("attack")
